b0dc4917bdc167045554d4bd0095c4f35f573e3b
vantung220902 <84801062+vantung220902@users.noreply.github.com>
2021-10-14 22:27:11 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
-
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <ColorModeScript resetCSS initialColorMode="light" />
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600&display=swap" rel="stylesheet"></link>
+      </Head>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
